45b72a44fab51119d715651e06f0441387c45afd
Roman Pavlenko <rpavlenko.mail@gmail.com>
2026-01-27 00:31:55 +0100

    feat: add main markup

--- scripts/initalizeSwiper.js
+++ scripts/initalizeSwiper.js
@@ -1,25 +1,25 @@
 (() => {
   if (
     !document.querySelector(
-      "#es-rc #es-rc-content #esColorsSwiper.swiper-initialized"
+      '#es-rc #es-rc-content #esColorsSwiper.swiper-initialized',
     )
   ) {
-    const swiper = new Swiper("#es-rc #es-rc-content #esColorsSwiper", {
+    const swiper = new Swiper('#es-rc #es-rc-content #esColorsSwiper', {
       enabled: false, // start disabled for mobile
       slidesPerView: 1,
       loop: true,
-      autoHeight: true,
+      // autoHeight: true,
       // autoplay: {
       //   delay: 5000,
       //   disableOnInteraction: true,
       // },
       pagination: {
-        el: "#es-swiper-pagination",
+        el: '#es-swiper-pagination',
         clickable: true,
       },
       navigation: {
-        nextEl: "#es-button-next",
-        prevEl: "#es-button-prev",
+        nextEl: '#es-button-next',
+        prevEl: '#es-button-prev',
       },
       breakpoints: {
         756: {
