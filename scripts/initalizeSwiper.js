fe1f6e294a801f5616b8248d878a8afcfd6356d2
Roman Pavlenko <rpavlenko.mail@gmail.com>
2026-01-27 01:40:51 +0100

    feat: modify markup and styles

--- scripts/initalizeSwiper.js
+++ scripts/initalizeSwiper.js
@@ -1,31 +1,20 @@
 (() => {
-  if (
-    !document.querySelector(
-      '#es-rc #es-rc-content #esColorsSwiper.swiper-initialized',
-    )
-  ) {
-    const swiper = new Swiper('#es-rc #es-rc-content #esColorsSwiper', {
-      enabled: false, // start disabled for mobile
-      slidesPerView: 1,
-      loop: true,
-      // autoHeight: true,
-      // autoplay: {
-      //   delay: 5000,
-      //   disableOnInteraction: true,
-      // },
-      pagination: {
-        el: '#es-swiper-pagination',
-        clickable: true,
+  const swiper = new Swiper('#es-rc #es-rc-content .es-swiper', {
+    enabled: false, // start disabled for mobile
+    slidesPerView: 1,
+    loop: true,
+    pagination: {
+      el: '#es-swiper-pagination',
+      clickable: true,
+    },
+    navigation: {
+      nextEl: '#es-button-next',
+      prevEl: '#es-button-prev',
+    },
+    breakpoints: {
+      756: {
+        enabled: true,
       },
-      navigation: {
-        nextEl: '#es-button-next',
-        prevEl: '#es-button-prev',
-      },
-      breakpoints: {
-        756: {
-          enabled: true,
-        },
-      },
-    });
-  }
+    },
+  });
 })();
